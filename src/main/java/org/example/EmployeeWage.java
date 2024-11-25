d79aeaafb071d9a773ec6922971608edba97477c
Akshay15561 <akshaynakawale12@gmail.com>
2024-11-25 17:46:34 +0530

    Added: uc1-check-employee

--- src/main/java/org/example/EmployeeWage.java
+++ src/main/java/org/example/EmployeeWage.java
@@ -1,11 +1,23 @@
 package org.example;
-
+import java.util.Random;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class EmployeeWage {
     public static void main(String[] args) {
 
-        System.out.printf("Welcome to Employee Wage Computation");
+        System.out.println("Welcome to Employee Wage Computation");
+        // Create a Random object
+        Random random = new Random();
+
+        // Generate a random number (0 or 1)
+        int attendance = random.nextInt(2); // 0 for Absent, 1 for Present
+
+        // Check attendance and display the result
+        if (attendance == 1) {
+            System.out.println("Present");
+        } else {
+            System.out.println("Absent");
+        }
 
 
 
