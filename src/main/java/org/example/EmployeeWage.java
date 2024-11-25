b2bd5f82ed7a5f830a7fcc6357338181981fa30d
Akshay15561 <akshaynakawale12@gmail.com>
2024-11-25 20:45:38 +0530

    Added: uc3-add-part-time employee wage

--- src/main/java/org/example/EmployeeWage.java
+++ src/main/java/org/example/EmployeeWage.java
@@ -7,20 +7,26 @@ public class EmployeeWage {
         // Constants
         final int WAGE_PER_HOUR = 20; // Example hourly wage
         final int FULL_DAY_HOURS = 8;
+        final int PART_TIME_HOURS = 4; // Hours for part-time
+
 
         System.out.println("Welcome to Employee Wage Computation");
         // Create a Random object
         Random random = new Random();
 
         // Generate a random number (0 or 1)
-        int attendance = random.nextInt(2); // 0 for Absent, 1 for Present
+        int attendance = random.nextInt(3); // 0 for Absent, 1 for Present
 
         // Check attendance and display the result
         if (attendance == 1) {
             System.out.println("Present");
             int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
             System.out.println("Daily Wage: $" + dailyWage);
-        } else {
+        } else if(attendance == 2) {
+            System.out.println("Present (Part-Time)");
+            int dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+            System.out.println("Daily Wage: $" + dailyWage);
+        } else{
             System.out.println("Absent");
             System.out.println("Daily Wage: $0");
         }
